1929ade0472f0aef8cafc47e63949c7a7e7bb5a0
Will <will_constable@msn.com>
2021-04-13 17:56:08 +0100

    Add auth

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,37 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link><span v-if="isLoggedIn"> | <a @click="logout">Logout</a></span>
     </div>
-    <router-view />
+    <router-view/>
   </div>
 </template>
+
+<script>
+  export default {
+    computed: {
+      isLoggedIn () {
+        return this.$store.getters.isLoggedIn
+      }
+    },
+    methods: {
+      logout () {
+        this.$store.dispatch('logout')
+        .then(() => this.$router.push('/login'))
+      }
+    },
+    created () {
+      this.$http.interceptors.response.use(undefined, function(err) {
+        return new Promise((resolve, reject) => {
+          if (err.status === 401 && err.config && !err.config.__isRetryRequest) {
+            this.$store.dispatch('logout')
+          }
+          throw err
+        })
+      })
+    }
+  }
+</script>
 
 <style>
 #app {
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import store from '../store'
+import Home from "../views/Home.vue"
+import About from '../views/About.vue'
+import Login from '../components/Login.vue'
+import Secure from '../components/Secure.vue'
+import Register from '../components/Register.vue'
 
 Vue.use(VueRouter);
 
@@ -11,18 +16,44 @@ const routes = [
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: '/login',
+    name: 'login',
+    component: Login
   },
-];
+  {
+    path: '/register',
+    name: 'register',
+    component: Register
+  },
+  {
+    path: '/secure',
+    name: 'secure',
+    component: Secure,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: About
+  }
+]
 
 const router = new VueRouter({
-  routes,
-});
+  routes
+})
 
-export default router;
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (store.getters.isLoggedIn) {
+      next()
+      return
+    }
+    next('/login')
+  } else {
+    next()
+  }
+})
+
+export default router
